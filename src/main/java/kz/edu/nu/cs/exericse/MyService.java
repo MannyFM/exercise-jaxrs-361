4c44aa95ad2b1fad020caf0760e94744a8d2f3c6
Alibek Manabayev <Alibek.manabayev@gmail.com>
2018-09-21 14:39:39 +0600

    done

--- src/main/java/kz/edu/nu/cs/exericse/MyService.java
+++ src/main/java/kz/edu/nu/cs/exericse/MyService.java
@@ -2,6 +2,7 @@ package kz.edu.nu.cs.exericse;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Pattern;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
@@ -23,14 +24,26 @@ public class MyService {
 
     @POST
     public Response createEntry(@FormParam("key") String key, @FormParam("value") String value) {
+        if (!isValidKey(key) || !isValidValue(value) || entries.containsKey(key))
+            return Response.status(Status.FORBIDDEN).build();
         entries.put(key, value);
         return Response.status(Status.CREATED).build();
     }
 
     @GET
-    @Path("{id: [a-zA-Z]+}")
-    public Response getById(@PathParam("id") String id) {
-        String r = "Requested for item with id \"" + id + "\"";
+    @Path("{key: \\w+}")
+    public Response getById(@PathParam("key") String key) {
+        String r = "Requested for key \"" + key + "\"";
+        if (!entries.containsKey(key))
+            return Response.status(Status.NOT_FOUND).build();
         return Response.ok(r).build();
+    }
+
+    private boolean isValidKey(String string) {
+        return string.matches("^\\w+$");
+    }
+
+    private boolean isValidValue(String string) {
+        return string.matches("^[\\w ]+$");
     }
 }
