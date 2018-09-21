a88c44cae2d0380d7709a5d8effdba473475b7e3
marks <marks1024@users.noreply.github.com>
2018-09-21 12:02:53 +0600

    edits

--- src/main/java/kz/edu/nu/cs/exericse/MyService.java
+++ src/main/java/kz/edu/nu/cs/exericse/MyService.java
@@ -3,9 +3,14 @@ package kz.edu.nu.cs.exericse;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import com.google.gson.Gson;
 
 @Path("/items")
 public class MyService {
@@ -14,7 +19,14 @@ public class MyService {
     
     @GET
     public Response getItems() {
-        String r = "JAX-RS: Hello, world";
-        return Response.ok(r).build();
+        Gson gson = new Gson();
+        String json = gson.toJson(entries);
+        return Response.ok(json).build();
+    }
+    
+    @POST
+    public Response createEntry(@FormParam("mykey") String mykey, @FormParam("myentry") String myentry) {
+        entries.put(mykey, myentry);
+        return Response.status(Status.CREATED).build();
     }
 }
