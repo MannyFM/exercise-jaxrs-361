d4adcd5a65e158e6d0a292ae723d853f59bc8471
Alibek Manabayev <Alibek.manabayev@gmail.com>
2018-09-21 14:22:07 +0600

    cleanup

--- src/main/java/kz/edu/nu/cs/exericse/MyApplication.java
+++ src/main/java/kz/edu/nu/cs/exericse/MyApplication.java
@@ -8,13 +8,13 @@ import javax.ws.rs.core.Application;
 
 @ApplicationPath("/services")
 public class MyApplication extends Application {
-    private Set<Object> singletons = new HashSet<Object>();
-    private Set<Class<?>> empty = new HashSet<Class<?>>();
-    
+    private Set<Object> singletons = new HashSet<>();
+    private Set<Class<?>> empty = new HashSet<>();
+
     public MyApplication() {
         singletons.add(new MyService());
     }
-    
+
     @Override
     public Set<Class<?>> getClasses() {
         return empty;
--- src/main/java/kz/edu/nu/cs/exericse/MyService.java
+++ src/main/java/kz/edu/nu/cs/exericse/MyService.java
@@ -3,10 +3,7 @@ package kz.edu.nu.cs.exericse;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
@@ -14,19 +11,26 @@ import com.google.gson.Gson;
 
 @Path("/items")
 public class MyService {
-    
-    private Map<String, String> entries = new ConcurrentHashMap<String, String>();
-    
+
+    private Map<String, String> entries = new ConcurrentHashMap<>();
+
     @GET
     public Response getItems() {
         Gson gson = new Gson();
         String json = gson.toJson(entries);
         return Response.ok(json).build();
     }
-    
+
     @POST
-    public Response createEntry(@FormParam("mykey") String mykey, @FormParam("myentry") String myentry) {
-        entries.put(mykey, myentry);
+    public Response createEntry(@FormParam("key") String key, @FormParam("value") String value) {
+        entries.put(key, value);
         return Response.status(Status.CREATED).build();
+    }
+
+    @GET
+    @Path("{id: [a-zA-Z]+}")
+    public Response getById(@PathParam("id") String id) {
+        String r = "Requested for item with id \"" + id + "\"";
+        return Response.ok(r).build();
     }
 }
